c87e038b3259bed7417802fae43ea2fff65a6c0a
Anuragbbd <anuragguru69@gmail.com>
2026-09-08 23:31:27 +0530

    Add order cancellation and status update features with validation

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -109,7 +109,7 @@ const getOrderById = asyncHandler(async (req, res) => {
             select: "username email"
         });
 
-        if (!order) {
+    if (!order) {
         throw new ApiError(404, "Order not found or you do not have permission to view it");
     }
 
@@ -125,8 +125,142 @@ const getOrderById = asyncHandler(async (req, res) => {
 });
 
 
+const cancelOrder = asyncHandler(async (req, res) => {
+    const userId = req.user._id;
+    const orderId = req.params.orderId;
+
+    if (!orderId || !mongoose.Types.ObjectId.isValid(orderId)) {
+        throw new ApiError(400, "Valid order id is required");
+    }
+
+    const order = await Order.findOne({
+        _id: orderId,
+        customer: userId
+    });
+
+    if (!order) {
+        throw new ApiError(404, "Order not found or you do not have permission to cancel the order");
+    }
+
+    if (order.orderStatus === 'cancelled') {
+        throw new ApiError(400, "This order is already cancelled");
+    }
+
+    if (order.orderStatus === 'shipped' || order.orderStatus === 'delivered') {
+        throw new ApiError(400, "Order cannot be cancelled once it is shipped or delivered");
+    }
+
+    for (const item of order.items) {
+        await Product.findByIdAndUpdate(
+            item.product,
+            { $inc: { stock: item.quantity } }
+        )
+    }
+
+    order.orderStatus = 'cancelled';
+
+    if (order.paymentStatus === 'paid') {
+        order.paymentStatus = 'refunded'
+    }
+
+    await order.save();
+
+    return res
+        .status(200)
+        .json(new ApiResponse(
+            200,
+            order,
+            "Order cancelled and stock restored successfully"
+        ));
+});
+
+
+const updateOrderStatus = asyncHandler(async (req, res) => {
+    const orderId = req.params.orderId;
+    const { newStatus } = req.body;
+    const { role, _id: userId }= req.user;
+
+    if (!orderId || !mongoose.Types.ObjectId.isValid(orderId)) {
+        throw new ApiError(400, "Valid order id is required");
+    }
+
+    if (role !== 'seller' && role !== 'admin') {
+        throw new ApiError(403, "Access denied: Admin and seller only");
+    }
+
+    const validStatuses = ['processing', 'shipeed', 'delivered', 'cancelled'];
+    if (!newStatus || !validStatuses.includes(newStatus.toLowerCase())) {
+        throw new ApiError(400, `Status must be one of: ${validStatuses.join(", ")}`);
+    }
+
+    const normalizedStatus = newStatus.toLowerCase();
+
+    const order = await Order.findById(orderId);
+    if (!order) {
+        throw new ApiError(404, "Order not found");
+    }
+
+    if (role === 'seller') {
+        const isSellerPresent = order.items.some(
+            (item) => item.seller?.toString() === userId.toString()
+        );
+
+        if (!isSellerPresent) {
+            throw new ApiError(403, "Forbidden: This order does not contain order from your store")
+        }
+    }
+
+    if (order.orderStatus === normalizedStatus) {
+        throw new ApiError(400, `Order is already marked as ${normalizedStatus}`);
+    }
+
+    if (order.orderStatus === 'cancelled') {
+        throw new ApiError(400, "Cannot update, Order was already cancelled");
+    }
+
+    if (order.orderStatus === 'delivered') {
+        throw new ApiError(400, "Cannot update. Order is already delivered");
+    }
+
+    if (order.orderStatus === 'processing' && normalizedStatus === 'delivered') {
+        throw new ApiError(400, "Order must be marked 'shiped' before it can be marked 'delivered' ");
+    }
+
+    if (normalizedStatus === 'cancelled') {
+        for (const item of order.items) {
+            await Product.findByIdAndUpdate(
+                item.product,
+                { $inc: { stock: item.quantity } }
+            );
+        }
+
+        if (order.paymentStatus === 'paid') {
+            order.paymentStatus === 'refunded'
+        }
+    }
+
+    if (normalizedStatus === 'delivered' && order.paymentStatus === 'pending') {
+        order.paymentStatus = 'paid';
+    }
+
+    order.orderStatus = normalizedStatus;
+    await order.save();
+
+    return res
+        .status(200)
+        .json(new ApiResponse(
+            200,
+            order,
+            `Order status sucessfully updated to ${normalizedStatus}`
+        ));
+
+});
+
+
 export {
     placeOrder,
     getMyOrders,
     getOrderById,
+    cancelOrder,
+    updateOrderStatus,
 }
--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import {jwtVerify} from "../middlewares/auth.middleware.js"
-import { getMyOrders, getOrderById, placeOrder } from "../controllers/order.controller.js";
+import { cancelOrder, getMyOrders, getOrderById, placeOrder, updateOrderStatus } from "../controllers/order.controller.js";
 
 
 const router = Router();
@@ -10,6 +10,8 @@ router.use(jwtVerify);
 router.route('/create-order').post(placeOrder);
 router.route('/my-orders').get(getMyOrders);
 router.route('/:orderId').get(getOrderById);
+router.route('/cancel-order/:orderId').patch(cancelOrder);
+router.route('/update-status/:orderId').patch(updateOrderStatus);
 
 
 export default router;
